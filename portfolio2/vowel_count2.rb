9525bd82c4c056a1aba7d86dbda007bda021f138
Ricardo da Paz <rdapaz@our.ecu.edu.au>
2019-03-24 05:12:38 +0800

    Some changes to ruby script, was not adding up correctly

--- portfolio2/vowel_count2.rb
+++ portfolio2/vowel_count2.rb
@@ -10,19 +10,26 @@ def countVowels(filepath)
 
     File.open(filepath, 'r') do |f|
       f.each_line do |line|
-        line.scan(/[\w'-]+/) do |word|
-            word.downcase!
-            word.scan(/[aeiou]/) do |vowel|
+        line.downcase!
+        line.scan(/[aeiou]/) do |vowel|
             unless vowelCounter.keys.include? vowel
               vowelCounter[vowel] = 0
-            else
-              vowelCounter[vowel] += 1
             end
-          end
+            vowelCounter[vowel] += 1
         end
       end
     end
-
+    # What if the vowel wasn't in the sample text? We would still want
+    # to display it in the chart.  This addresses that need
+    # Note that %w{a e i o u} = ['a', 'b', 'c', 'd', 'e'] but it is 
+    # easier to type
+    %w{a e i o u}.each do |vowel|
+        # To mix it up a bit we will use the ! not operator 
+        # rather than the unless conditional
+        if ! vowelCounter.keys.include? vowel
+            vowelCounter[vowel] = 0
+        end
+    end
     vowelCounter = Hash[ vowelCounter.sort_by { |k, v| k } ]
 
     vowelCounter
@@ -33,21 +40,17 @@ def barChart(countHash, maxSymbols, symbol)
     maxEntry = countHash.max_by {|k, v| v}
     maxScore = maxEntry[1]
     countHash.each do |item, count|
-        puts "#{item}: " + symbol*(count*maxSymbols/maxScore).to_i
+        puts "#{item}: " + symbol*(count*maxSymbols/maxScore).to_i + " (#{count})"
     end
 end
 
 
 if __FILE__ == $0
-    MAX_COUNT = 20
-    DEBUG = false
+    MAX_COUNT = 100
+    DEBUG = true
     path = '/home/rdapaz/projects/CSG6206/portfolio2/resources/lorem.txt'
     h = countVowels(path)
     barChart(h, MAX_COUNT, '*')
-
-    if DEBUG
-      h.each {|k, v| puts "#{k}: #{v}"}
-    end
 end
 
 
