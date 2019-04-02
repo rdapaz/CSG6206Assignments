1e61ff8e7fd02a422d79f9567a834398f11b779d
Ricardo da Paz <rdapaz@our.ecu.edu.au>
2019-04-02 23:28:14 +0800

    Tidied up and created comments

--- portfolio2/vowel_count.rb
+++ portfolio2/vowel_count.rb
@@ -6,48 +6,93 @@ def countVowels(filepath)
     # This program will count the number of vowels read from a text
     # file where the path is passed in as the argument
 
+    # We first initialise a hash to hold our vowels and their
+    # respective count
     vowelCounter = {}
+
+    # Here we open the file referenced by the filepath variable
+    # read all the lines, turning them into lowercase
 
     File.open(filepath, 'r') do |f|
       f.each_line do |line|
-        line.scan(/[\w'-]+/) do |word|
-            word.downcase!
-            word.scan(/[aeiou]/) do |vowel|
+        line.downcase!
+        # The code below looks for individual occurrences
+        # of each vowel in each line if we have already
+        # initialised our counter for that vowel, the counter
+        # is incremented, otherwise we initialise the counter
+        # and set it to 0
+        line.scan(/[aeiou]/) do |vowel|
             unless vowelCounter.keys.include? vowel
               vowelCounter[vowel] = 0
-            else
-              vowelCounter[vowel] += 1
             end
-          end
+            vowelCounter[vowel] += 1
         end
       end
     end
-
-    vowelCounter = Hash[ vowelCounter.sort_by { |k, v| k } ]
+    # What if the vowel wasn't in the sample text? We would still want
+    # to display it in the chart.  This addresses that need
+    # Note that %w{a e i o u} = ['a', 'b', 'c', 'd', 'e'] but it is 
+    # easier to type
+    %w{a e i o u}.each do |vowel|
+        # To mix it up a bit we will use the ! not operator 
+        # rather than the unless conditional
+        if ! vowelCounter.keys.include? vowel
+            vowelCounter[vowel] = 0
+        end
+    end
+    vowelCounter = vowelCounter.sort_by { |keys, values| keys }.to_h
 
     vowelCounter
 end
 
 
-def barChart(countHash, maxSymbols, symbol)
-    maxEntry = countHash.max_by {|k, v| v}
+def barChart(countHash, maxSymbols, symbol, debug)
+    maxEntry = countHash.max_by {|keys, values| values}
+    # maxEntry is comprised of an array containing the maximum key and its count
+    # since we are only interested in the key, we take the second element of that
+    # array = maxEntry[1]
     maxScore = maxEntry[1]
+    # Next we represent the count using the following algorithm:
+    # (a) Determine the largest count
+    # (b) The element at the key with the highest cound will be represented with
+    #     the maximum number of symbols using the following equation
+    #     number of symbols = Integer((count * maxSymbols)/maxScore)
+    #     Hence if the count is the maxScore, we will get maxSymbols
+    # (c) In ruby '*' x 10 = '**********' so we then substitute '*' with 
+    #     symbox as we allow the user to change how this is displayed
     countHash.each do |item, count|
-        puts "#{item}: " + symbol*(count*maxSymbols/maxScore).to_i
+        # Next we display the textual graphic representation of the count
+        # by doing variable interpolation ruby style.  Note that we 
+        # have optionally allowed a count to be displayed at the end
+        # if debug is true.
+        # The portion "#{debug ? '(' + count.to_s + ')' : ''} either
+        # displays (10) if count is 10 and debug is true or nothing
+        puts "#{item}: #{symbol*(count*maxSymbols/maxScore).to_i}" + "#{debug ? ' ' + count.to_s : ''}"
     end
 end
 
-
+# The next line is the main program invocation.  Up until now
+# we had functions which could have been included in any program
+# however the if __FILE__ = $0 literally means if this is being
+# executed as a program (rather than having methods simplymixed in)
+# in another program then execute the lines below
 if __FILE__ == $0
-    MAX_COUNT = 20
-    DEBUG = false
-    path = '/home/rdapaz/projects/CSG6206/portfolio2/resources/lorem.txt'
-    h = countVowels(path)
-    barChart(h, MAX_COUNT, '*')
-
-    if DEBUG
-      h.each {|k, v| puts "#{k}: #{v}"}
-    end
+    # We initially define some constants
+    MAX_COUNT = 5
+    DEBUG = true
+    PATH  = '/home/rdapaz/projects/CSG6206/portfolio2/resources/lorem.txt'
+    # We generate a hash, h of the vowelcount by
+    # invoking our countVowels function
+    h = countVowels(PATH)
+    # We generate the bar chart by calling the 
+    # barChart function and passing in the appropriate
+    # parameters, namely the hash variable containing
+    # the counts for each vowel, the maximum
+    # number of symbols to be displayed, the symbol to he
+    # used in the representation and a DEBUG variable
+    # indicating whether the count is to be displayed
+    # next to the symbol representation
+    barChart(h, MAX_COUNT, '*', DEBUG)
 end
 
 
