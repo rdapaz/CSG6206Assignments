7a4b678cb4a4d891723c8a5d90d577844c78b4ba
Ricardo da Paz <rdapaz@our.ecu.edu.au>
2019-05-05 06:02:10 +0800

    changed sql file

--- portfolio3/query.sql
+++ portfolio3/query.sql
@@ -1,3 +1,4 @@
+-- Sample query to prove that our count is right
 select Gender, count(Gender) from demographics where Age >=16 and Gender='Male' union
 select Gender, count(Gender) from demographics where Age <16 and Gender='Male' union
 select Gender, count(Gender) from demographics where Age >=16 and Gender='Female' union
